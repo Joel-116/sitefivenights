11c9017655b1d3aeb31d81a247c49dde12c7cc26
João Emanoel <137958162+Joel-116@users.noreply.github.com>
2024-08-12 15:48:57 -0300

    Update signup.test.js

--- freddyfazbear/signup.test.js
+++ freddyfazbear/signup.test.js
@@ -1,41 +1,33 @@
-const { validatePassword, validateConfirmPassword, validateAge } = require('./signup');
+const { validatePassword, validateAge } = require('./signup');
 
-test('Teste de senha com regras corretas', () => {
-    const result = validatePassword('Password123!');
-    expect(result).toBe(true); 
+it('Teste de senha com regras corretas', () => {
+    expect(validatePassword('Password123!')).toBe(true);
 });
 
-test('Teste de senha com menos de 3 caracteres', () => {
-    const result = validatePassword('a1');
-    expect(result).toBe(false);
+it('Teste de senha com menos de 3 caracteres', () => {
+    expect(validatePassword('a1')).toBe(false);
 });
 
-test('Teste de senha faltando número', () => {
-    const result = validatePassword('Password!');
-    expect(result).toBe(false);
+it('Teste de senha faltando número', () => {
+    expect(validatePassword('Password!')).toBe(false);
 });
 
-test('Teste de senha faltando letra', () => {
-    const result = validatePassword('12345!');
-    expect(result).toBe(false);
+it('Teste de senha faltando letra', () => {
+    expect(validatePassword('12345!')).toBe(false);
 });
 
-test('Teste de senha faltando símbolo', () => {
-    const result = validatePassword('Password123');
-    expect(result).toBe(false);
+it('Teste de senha faltando símbolo', () => {
+    expect(validatePassword('Password123')).toBe(false);
 });
 
-test('Teste de idade maior ou igual a 18', () => {
-    const result = validateAge(18);
-    expect(result).toBe(true); 
+it('Teste de idade maior ou igual a 18', () => {
+    expect(validateAge(18)).toBe(true);
 });
 
-test('Teste de idade menor que 18', () => {
-    const result = validateAge(17);
-    expect(result).toBe(false);
+it('Teste de idade menor que 18', () => {
+    expect(validateAge(17)).toBe(false);
 });
 
-test('Teste de idade maior que 150', () => {
-    const result = validateAge(151);
-    expect(result).toBe(false);
+it('Teste de idade maior que 150', () => {
+    expect(validateAge(151)).toBe(false);
 });
